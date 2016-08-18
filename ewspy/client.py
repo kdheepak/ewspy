edacedd58c7b7c71fa6d078705fbb3e8cd71f4cc
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-08-17 19:42:50 -0600

    Add EWSService

--- ewspy/client.py
+++ ewspy/client.py
@@ -2,18 +2,16 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from builtins import (bytes, str, open, super, range,
                       zip, round, input, int, pow, object)
 
-from .session import EWSSession
+from .service import EWSService
 from .exception import EWSException
 
 
 class EWSClient(object):
 
-    def __init__(self, session=None,
-                 url=None,
-                 username=None,
-                 password=None):
+    def __init__(self, service):
 
-        if session is not None and not isinstance(session, EWSSession):
-            raise EWSException('Session has to be of instance EWSSession. Check documentation for more information')
+        if not isinstance(service, EWSService):
+            raise EWSException('Service has to be of instance EWSService. Check documentation for more information')
 
-        self._session = EWSSession(url, username, password)
+        self._service = service
+
